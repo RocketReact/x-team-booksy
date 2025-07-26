e54476a66b323f29783a0969b4768d72666f3702
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-26 14:38:33 +0300

    table-desktop

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper/bundle';
-import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+import { Navigation, Pagination, Keyboard} from 'swiper/modules';
 import 'swiper/css/bundle';
 import '../css/feedbacks.css'
  const swiper = new Swiper('.swiper', {
@@ -7,11 +7,22 @@ import '../css/feedbacks.css'
         slidesPerView: 1,
         slidesPerGroup: 1,
         centeredSlides: false,
-
         simulateTouch: true,
         touchRatio: 1,
         spaceBetween: 0,
         grabCursor: true,
+        breakpoints: {
+         768: {
+             slidesPerView: 2,   
+             spaceBetween: 24,
+             slidesPerGroup: 2,
+         },
+         1200: {
+             slidesPerView: 3,
+             spaceBetween: 24,
+             slidesPerGroup: 3,
+         },
+        },
         pagination: {
             el: '.custom-swiper-pagination',
             type: 'bullets',
