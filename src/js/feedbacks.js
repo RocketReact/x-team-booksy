695a9d2e82e61aff3f41ee624c3bda41f61bd81f
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-07-26 11:36:40 +0300

    swiper

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,36 +1,34 @@
-// Import Swiper and modules
 import Swiper from 'swiper/bundle';
 import { Navigation, Pagination, Keyboard } from 'swiper/modules';
 import 'swiper/css/bundle';
+import '../css/feedbacks.css'
 
-
-export const swiper = new Swiper ('.swiper', {
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 1,
-    simulateTouch: true, 
-    touchRatio: 1, 
-    spaceBetween: 24,
-    grabCursor: true,
-  
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-      navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    disabledClass: 'swiper-button-disabled',
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    },
-
-    modules: [Navigation, Pagination, Keyboard],
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        loopedSlides: 2,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        simulateTouch: true,
+        touchRatio: 1,
+        spaceBetween: 24,
+        grabCursor: true,
+        pagination: {
+            el: '.custom-swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.custom-swiper-button-next',
+            prevEl: '.custom-swiper-button-prev',
+            disabledClass: 'custom-swiper-button-disabled',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        modules: [Navigation, Pagination, Keyboard],
+    });
 });
-
-
-
 
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import "modern-normalize";
 import 'swiper/css';
+import './js/feedbacks.js';
