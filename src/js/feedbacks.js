bd5b501d5d42e61357070c6516fc74a02d7efc52
Oleg Shapoval <olegkazhan@gmail.com>
2025-07-27 10:13:28 +0300

    fix

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,46 +1,46 @@
 import Swiper from 'swiper/bundle';
-import { Navigation, Pagination, Keyboard} from 'swiper/modules';
+import { Navigation, Pagination, Keyboard } from 'swiper/modules';
 import 'swiper/css/bundle';
-import '../css/feedbacks.css'
- const swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        centeredSlides: false,
-        simulateTouch: true,
-        touchRatio: 1,
-        spaceBetween: 0,
-        grabCursor: true,
-        watchOverflow: true,
-        speed: 250,
-        breakpoints: {
-         768: {
-             slidesPerView: 2,   
-             spaceBetween: 24,
-             slidesPerGroup: 1,
-         },
-         1200: {
-             slidesPerView: 3,
-             spaceBetween: 24,
-             slidesPerGroup: 1,
-         },
-        },
-        pagination: {
-            el: '.custom-swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.custom-swiper-button-next',
-            prevEl: '.custom-swiper-button-prev',
-            disabledClass: 'custom-swiper-button-disabled',
-        },
-        keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-        },
-        modules: [Navigation, Pagination, Keyboard],
-    });
+import '../css/feedbacks.css';
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  centeredSlides: false,
+  simulateTouch: true,
+  touchRatio: 1,
+  spaceBetween: 0,
+  grabCursor: true,
+  watchOverflow: true,
+  speed: 250,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+      slidesPerGroup: 1,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      slidesPerGroup: 1,
+    },
+  },
+  pagination: {
+    el: '.custom-swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.custom-swiper-button-next',
+    prevEl: '.custom-swiper-button-prev',
+    disabledClass: 'custom-swiper-button-disabled',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  modules: [Navigation, Pagination, Keyboard],
+});
 
 // swiper.on('slideChange', () => {
 //   console.log('Current active slide index:', swiper.activeIndex);
