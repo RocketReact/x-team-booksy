a97b646dd4e8a01c0986e01b625df038a98a0c67
Yehor <qugar04@gmail.com>
2025-07-28 23:06:34 +0300

    fi

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import 'modern-normalize';
 import './js/events.js';
 import './js/contact-modal.js';
-<<<<<<< HEAD
-import './js/api.js';
-import './js/render-functions.js';
-=======
 import './js/header.js';
 import './js/feedbacks.js';
 import './js/hero-swiper';
 import './js/hero';
->>>>>>> 9bf1d4866e3e6fd2df123fe01ed1e0e2731f810d
 import './js/books.js';
 import './js/scroll-up.js';
 import './js/footer.js';
